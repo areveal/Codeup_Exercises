36fd2cc0adced05f62bc80069b3041d6428a04ca
Cole Reveal <alexi.reveal@gmail.com>
2014-05-09 11:49:50 -0500

    worked on if else practice file

--- if.php
+++ if.php
@@ -1,46 +1,56 @@
-<?php 
-	
-$a = 5;
-$b = 10;
-$c = '10';
+<?php
 
+$a = 7;
+$b = 7;
+$c = 3;
+
+// Add an else clause to the next two statemets
 if ($a < $b) {
     // output the appropriate result
-    echo "$a is less than $b\n";
+    echo "$a is less than $b";
+} else {
+    echo "$a is not less than $b";
 }
+
+echo "\n";
 
 if ($b > $a) {
     // output the appropriate result
-    echo "$b is greater than $a\n";
+    echo "$b is greater than $a";
+} else {
+    echo "$b is not greater than $a";
 }
-
+echo "\n";
+// Shorten the next 2 statements into an if/else
 if ($b >= $c) {
     // output the appropriate result
     echo "$b is greater than or equal to $c\n";
+} else {
+    echo "$b is less than $c\n";
 }
 
 if ($b <= $c) {
     // output the appropriate result
     echo "$b is less than or equal to $c\n";
+} else {
+    echo "$b is greater than $c\n";
 }
 
+// combine the next 4 conditionals into 
+// one if/else/elseif block that checks in order for:
+// identical, equal, not identical, not equal
 if ($b == $c) {
     // output the appropriate result
-    echo "$b is equalivalent to $c\n";
-}
-
-if ($b === $c) {
+    echo "$b is equal to $c\n";
+} else if ($b === $c) {
     // output the appropriate result
-    echo "$b is identical $c\n";
-}
-
-// output the appropriate result
-if ($b != $c) {
+    echo "$b is identical to $c\n";
+} else if ($b != $c) {
+    // output the appropriate result
     echo "$b is not equal to $c\n";
-}
-
-// output the appropriate result
-if ($b !== $c) {
+} else if ($b !== $c) {
+    // output the appropriate result
     echo "$b is not identical to $c\n";
 }
+
 ?>
