4844ad934ebcab446ac10214f1d5dbeedb7a7633
Cole Reveal <alexi.reveal@gmail.com>
2014-05-13 10:41:04 -0500

    completed FizzBuzz and for-incrementer files

---
+++ fizzbuzz.php
@@ -0,0 +1,19 @@
+<?php
+
+for($a=1;$a<=100;$a++) {
+	if(($a % 3 ==0) && ($a % 5 ==0)) {
+		echo "FizzBuzz\n";
+	} elseif($a % 3 ==0) {
+		echo "Fizz\n";
+	} elseif ($a %5 ==0) {
+		echo "Buzz\n";
+	} else {
+		echo $a . PHP_EOL;
+	}
+}
+
+
+
+
+
+?>
---
+++ for.php
@@ -0,0 +1,42 @@
+<?php
+
+echo "Please enter a number. ";
+
+$num1 = trim(fgets(STDIN));
+
+echo "Please enter a different number. ";
+
+$num2 = trim(fgets(STDIN));
+
+if(!is_numeric($num1)||!is_numeric($num2)) {
+	echo "You must enter two numbers.";
+	exit(1);
+}
+
+if($num1 == $num2) {
+	echo "You must enter two different numbers.";
+	exit(1);
+}
+
+echo "What would you like to increment by? ";
+
+$inc = trim(fgets(STDIN));
+
+if($inc == 0) {
+	$inc = 1;
+	echo "Not sure what you meant by that but we'll use 1.\n";
+}
+
+if($num1<$num2) {
+	for($a = $num1; $a <= $num2; $a += $inc) {
+		echo "$a ";
+	}
+} else {
+	for($a = $num1; $a >= $num2; $a -= $inc) {
+		echo "$a ";
+	}
+}
+
+echo PHP_EOL;
+
+?>
