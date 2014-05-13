0f2336710ce1f1ca28cb4b2b694500f34423b17a
Cole Reveal <alexi.reveal@gmail.com>
2014-05-13 10:47:08 -0500

    added notes to the fizzbuzz and for files

--- fizzbuzz.php
+++ fizzbuzz.php
@@ -1,6 +1,11 @@
 <?php
 
+// Let's begin
 for($a=1;$a<=100;$a++) {
+	// If our number is divisible by both 3 and 5, we will ouput "FizzBuzz".
+	// Else if our number is divisble only by 3, we will ouput "Fizz".
+	// Else if our number is divisible only by 5, we will output "Buzz".
+	//Else we will simply ouput the number itself.
 	if(($a % 3 ==0) && ($a % 5 ==0)) {
 		echo "FizzBuzz\n";
 	} elseif($a % 3 ==0) {
--- for.php
+++ for.php
@@ -1,5 +1,6 @@
 <?php
 
+// Let's begin.
 echo "Please enter a number. ";
 
 $num1 = trim(fgets(STDIN));
@@ -8,16 +9,19 @@ echo "Please enter a different number. ";
 
 $num2 = trim(fgets(STDIN));
 
+// Check to make sure both values were numbers.
 if(!is_numeric($num1)||!is_numeric($num2)) {
 	echo "You must enter two numbers.";
 	exit(1);
 }
 
+// Check to make sure the numbers were two different numbers.
 if($num1 == $num2) {
 	echo "You must enter two different numbers.";
 	exit(1);
 }
 
+// Create the incrementer.
 echo "What would you like to increment by? ";
 
 $inc = trim(fgets(STDIN));
@@ -27,6 +31,7 @@ if($inc == 0) {
 	echo "Not sure what you meant by that but we'll use 1.\n";
 }
 
+// Here is the actual output. Increment or decrement?
 if($num1<$num2) {
 	for($a = $num1; $a <= $num2; $a += $inc) {
 		echo "$a ";
