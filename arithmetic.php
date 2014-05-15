dbcd0f26247948426ccd5efc4f093fcc708a995d
Cole Reveal <alexi.reveal@gmail.com>
2014-05-15 09:45:06 -0500

    created arithmetic functions for arithmetic file

--- arithmetic.php
+++ arithmetic.php
@@ -1,19 +1,25 @@
 <?php
 
 function add($a, $b) {
-    echo $a + $b;
+    echo $a + $b . PHP_EOL;
 }
 
 function subtract($a, $b) {
-    // Add code here
+	echo $a - $b . PHP_EOL;	
 }
 
 function multiply($a, $b) {
-    // Add code here
+    echo $a * $b . PHP_EOL;
 }
 
 function divide($a, $b) {
-    // Add code here
+    echo $a / $b . PHP_EOL;
 }
+
+add(1,3);
+subtract(5,2);
+multiply(2,5);
+divide(10,5);
+
 
 ?>
