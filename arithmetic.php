0a56ba07689468eb17e910c67b4f8a49b8598af1
Cole Reveal <alexi.reveal@gmail.com>
2014-05-15 14:02:08 -0500

    added error functions

--- arithmetic.php
+++ arithmetic.php
@@ -1,11 +1,22 @@
 <?php
 
+
+
+function diverror($a, $b) {
+    	echo "ERROR: You cannot divide by $b." . PHP_EOL;
+}
+
+function numerror($a, $b) {
+	echo 'ERROR: Both $a and $b must be numeric.' . PHP_EOL;
+    var_dump($a) . var_dump($b) . PHP_EOL;
+}
+
+
 function add($a, $b) {
 	if (is_numeric($a) && is_numeric($b)) {
     	echo $a + $b . PHP_EOL;
     } else {
-    	echo 'ERROR: Both $a and $b must be numeric.' . PHP_EOL;
-    	var_dump($a) . var_dump($b) . PHP_EOL;
+    	numerror($a, $b);
     }
 }
 
@@ -13,8 +24,7 @@ function subtract($a, $b) {
 	if (is_numeric($a) && is_numeric($b)) {
     	echo $a - $b . PHP_EOL;
     } else {
-    	echo 'ERROR: Both $a and $b must be numeric.' . PHP_EOL;
-    	var_dump($a) . var_dump($b) . PHP_EOL;
+    	numerror($a, $b);
     }	
 }
 
@@ -22,40 +32,35 @@ function multiply($a, $b) {
     if (is_numeric($a) && is_numeric($b)) {
     	echo $a * $b . PHP_EOL;
     } else {
-    	echo 'ERROR: Both $a and $b must be numeric.' . PHP_EOL;
-    	var_dump($a) . var_dump($b) . PHP_EOL;
+    	numerror($a, $b);
     }
 }
 
 function divide($a, $b) {
     if ($b == 0) {
-    	echo "ERROR: You cannot divide by $b." . PHP_EOL;
-    	break;
+    	diverror($a, $b);
     } 
     if (is_numeric($a) && is_numeric($b)) {
     	echo $a / $b . PHP_EOL;
     } else {
-    	echo 'ERROR: Both $a and $b must be numeric.' . PHP_EOL;
-    	var_dump($a) . var_dump($b) . PHP_EOL;
+    	numerror($a, $b);
     }
 }
 
 function modulo($a, $b) {
 	if ($b == 0) {
-    	echo "ERROR: You cannot divide by $b." . PHP_EOL;
-    	break;
+    	diverror($a, $b);
     } 
     if (is_numeric($a) && is_numeric($b)) {
     	echo $a % $b . PHP_EOL;
     } else {
-    	echo 'ERROR: Both $a and $b must be numeric.' . PHP_EOL;
-    	var_dump($a) . var_dump($b) . PHP_EOL;
+    	numerror($a, $b);
 
     }
 }
 
 
-add(1,'3');
+add(1,'hello');
 
 
 
