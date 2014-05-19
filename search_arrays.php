21a363570f7ac7565e7c5dd6ae661bd27be42cc0
Cole Reveal <alexi.reveal@gmail.com>
2014-05-19 13:42:03 -0500

    created function check in search arrays file

--- search_arrays.php
+++ search_arrays.php
@@ -4,3 +4,14 @@
 $names = ['Tina', 'Dana', 'Mike', 'Amy', 'Adam'];
 
 $compare = ['Tina', 'Dean', 'Mel', 'Amy', 'Michael'];
+
+function check($name, $list) {
+	if(array_search($name, $list) !== false){
+		return true;
+	} else {
+		return false;
+	}
+}
+
+
+?>
