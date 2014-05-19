473ffcc92bd573f6172cecdb6646dfc26c48608a
Cole Reveal <alexi.reveal@gmail.com>
2014-05-19 13:47:23 -0500

    created compare function for search arrays file

--- search_arrays.php
+++ search_arrays.php
@@ -3,7 +3,7 @@
 // first names
 $names = ['Tina', 'Dana', 'Mike', 'Amy', 'Adam'];
 
-$compare = ['Tina', 'Dean', 'Mel', 'Amy', 'Michael'];
+$compare = ['Tina', 'Dana', 'Mel', 'Amy', 'Michael'];
 
 function check($name, $list) {
 	if(array_search($name, $list) !== false){
@@ -12,6 +12,21 @@ function check($name, $list) {
 		return false;
 	}
 }
+
+function compare($array1, $array2) {
+	
+	$common = 0;
+
+	foreach ($array1 as $name) {
+	 	if(array_search($name, $array2) !== false) {
+	 		$common +=1;
+	 	}	
+	} 
+	return $common;
+}
+
+$how_many_same = compare($names, $compare);
+echo $how_many_same . PHP_EOL;
 
 
 ?>
