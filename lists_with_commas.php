4607577b7fced8dcf96f5bd4a2cb89646fb862a9
Cole Reveal <alexi.reveal@gmail.com>
2014-05-20 12:13:25 -0500

    added function into explode implode file

--- lists_with_commas.php
+++ lists_with_commas.php
@@ -1,14 +1,28 @@
 <?php
 
+function humanized_list($list){
+
+	$array = explode(', ', $list);
+	
+	//add 'and ' to last element of string
+	$new_last = 'and ' . array_pop($array);
+	array_push($array, $new_last);
+	
+	$new_string = implode(', ', $array);
+
+	return $new_string;
+
+}
+
+
+
+
+
+
+
 $physicists_string = 'Gordon Freeman, Samantha Carter, Sheldon Cooper, Quinn Mallory, Bruce Banner, Tony Stark';
 
-$physicists_array = explode(', ', $physicists_string);
-
-//add 'and ' to last element of string
-$new_last = 'and ' . array_pop($physicists_array);
-array_push($physicists_array, $new_last);
-
-$famous_fake_physicists = implode(', ', $physicists_array);
+$famous_fake_physicists = humanized_list($physicists_string);
 
 echo "Some of the most famous fictional theoretical physicists are {$famous_fake_physicists}.\n";
 
